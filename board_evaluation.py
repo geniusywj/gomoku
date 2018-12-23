ec202d5b3178b1ab6dfc7d72d12299292febafb0
Unknown <xuezha1996@gmail.com>
2018-12-23 14:42:14 +0800

    fix bugs for count continuous x

--- board_evaluation.py
+++ board_evaluation.py
@@ -6,14 +6,26 @@ def count_continuous_x(board, player, x):
     board_size = len(board)
     offsets = ((0, 1), (1, 1), (1, 0), (1, -1))
     count = 0
-    for i, j in product(range(board_size - x + 1), range(board_size - x + 1)):
+    for i, j in product(range(board_size), range(board_size)):
         if board[i][j] == player:
             for offset in offsets:
-                if i - offset[0] >= 0 and j - offset[1] >= 0 and board[i - offset[0]][j - offset[0]] == player:
+                if not out_of_bound(board, i - offset[0], j - offset[1]) and \
+                        board[i - offset[0]][j - offset[1]] == player:
                     continue
-                if all(board[i + offset[0] * x_][j + offset[1] * x_] == player for x_ in range(1, x)):
-                    if i + offset[0] * x < board_size and j + offset[1] * x < board_size:
-                        count += board[i + offset[0] * x][j + offset[1] * x] != player
-                    else:
-                        count += 1
+                legal = True
+                for x_ in range(1, x):
+                    i_ = i + offset[0] * x_
+                    j_ = j + offset[1] * x_
+                    if out_of_bound(board, i_, j_):
+                        legal = False
+                        break
+                    legal = legal and board[i_][j_] == player
+                if not out_of_bound(board, i + offset[0] * x, j + offset[1] * x) and \
+                        board[i + offset[0] * x][j + offset[1] * x] == player:
+                    legal = False
+                count += legal
     return count
+
+
+def out_of_bound(board, i, j):
+    return i >= len(board) or j >= len(board) or i < 0 or j < 0
--- test.py
+++ test.py
@@ -20,10 +20,12 @@ class TestCountX(TestCase):
             [0, 1, 1, 0],
             [0, 0, 1, 1]
         ]
-        result = count_continuous_x(board, 1, 3)
-        self.assertEqual(1, result)
+        result_three = count_continuous_x(board, 1, 3)
+        result_four = count_continuous_x(board, 1, 4)
+        self.assertEqual(1, result_three)
+        self.assertEqual(1, result_four)
 
-    def count_multiple_three(self):
+    def test_full_board_three(self):
         board = [
             [1, 1, 1],
             [1, 1, 1],
@@ -31,3 +33,15 @@ class TestCountX(TestCase):
         ]
         result = count_continuous_x(board, 1, 3)
         self.assertEqual(8, result)
+
+    def test_full_board_four(self):
+        board = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1]
+        ]
+        result_three = count_continuous_x(board, 1, 3)
+        result_four = count_continuous_x(board, 1, 4)
+        self.assertEqual(4, result_three)
+        self.assertEqual(10, result_four)
